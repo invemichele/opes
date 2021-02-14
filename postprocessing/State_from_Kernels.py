86ca034abaab4b16142ccfd2b4a851286f7c4bfe
Michele Invernizzi <inve.michele@gmail.com>
2021-02-14 20:12:15 +0100

    small improvements

--- postprocessing/State_from_Kernels.py
+++ postprocessing/State_from_Kernels.py
@@ -11,6 +11,7 @@ import argparse
 import subprocess
 plumed_exe='plumed'
 
+error='--- ERROR: %s '
 #parser
 parser = argparse.ArgumentParser(description='Generate an OPES STATE file from a KERNELS file, so that it can be used with FES_from_State.py. DO NOT use the obtained STATE file for restart')
 parser.add_argument('--kernels','-f',dest='filename',type=str,default='KERNELS',help='the kernels file name, with the deposited kernels')
@@ -25,39 +26,41 @@ outfile=args.outfile
 f=open(filename,'r')
 line=f.readline() #fields
 if line.split()[1]!='FIELDS':
-  sys.exit(' no FIELDS found in file "'+filename+'"')
+  sys.exit(error%(' no FIELDS found in file "'+filename+'"'))
 if len(line.split())<7:
-  sys.exit(' not enough FIELDS found in file "'+filename+'"')
+  sys.exit(error%(' not enough FIELDS found in file "'+filename+'"'))
 if (len(line.split())-5)%2!=0:
-  sys.exit(' wrong number of FIELDS found in file "'+filename+'"')
+  sys.exit(error%(' wrong number of FIELDS found in file "'+filename+'"'))
 ncv=int((len(line.split())-5)/2)
 cvname=[]
 for i in range(ncv):
   cvname.append(line.split()[3+i])
   if cvname[i].find('.')!=-1:
-    sys.exit(' %s: unfourtunately, you must modify the KERNELS file and remove any "." from CVs names'%cvname[i])
+    sys.exit(error%(' %s: you must modify the KERNELS file and remove any "." from CVs names'%cvname[i]))
+  if line.split()[4+i]!='sigma_'+cvname[i]:
+    sys.exit(error%(' expected "sigma_%s" instead of "%s"'%(cvname[i],line.split()[4+i])))
 line=f.readline() #action
 if line.split()[3]=='OPES_METAD_kernels':
   action='OPES_METAD'
 elif line.split()[3]=='OPES_METAD_EXPLORE_kernels':
   action='OPES_METAD_EXPLORE'
 else:
-  sys.exit(' this script only works with OPES_METAD or OPES_METAD_EXPLORE KERNELS files')
+  sys.exit(error%(' this script only works with OPES_METAD or OPES_METAD_EXPLORE KERNELS files'))
 line=f.readline() #biasfactor
 if line.split()[2]!='biasfactor':
-  sys.exit(' biasfactor not found!')
+  sys.exit(error%(' biasfactor not found!'))
 biasfactor=line.split()[3]
 line=f.readline() #epsilon
 if line.split()[2]!='epsilon':
-  sys.exit(' epsilon not found!')
+  sys.exit(error%(' epsilon not found!'))
 epsilon=line.split()[3]
 line=f.readline() #kernel_cutoff
 if line.split()[2]!='kernel_cutoff':
-  sys.exit(' kernel_cutoff not found!')
+  sys.exit(error%(' kernel_cutoff not found!'))
 kernel_cutoff=line.split()[3]
 line=f.readline() #compression_threshold
 if line.split()[2]!='compression_threshold':
-  sys.exit(' compression_threshold not found!')
+  sys.exit(error%(' compression_threshold not found!'))
 compression_threshold=line.split()[3]
 periodic=['NO']*ncv
 line=f.readline()
@@ -67,13 +70,13 @@ while line.split()[0]=='#!':
       periodic[i]=line.split()[3]+','
       line=f.readline()
       if line.split()[2]!='max_'+cvname[i]:
-        sys.exit(' periodic CVs should have both min and max value!')
+        sys.exit(error%(' periodic CVs should have both min and max value!'))
       periodic[i]+=line.split()[3]
   line=f.readline()
 f.close()
 
 #create temporary plumed file
-plumed_input='# vim:ft=plumed\n'
+plumed_input='# vim:ft=plumed\nUNITS NATURAL\n'
 plumed_input+='RESTART\n'
 plumed_input+='f: FIXEDATOM AT=0,0,0\n' #fake atom
 plumed_input+='d: DISTANCE ATOMS=f,f\n' #unfourtunately the FAKE colvar has issues with PERIODIC
@@ -81,7 +84,6 @@ plumed_input+='COMMITTOR ARG=d BASIN_LL1=-1 BASIN_UL1=1\n' #this will kill the d
 for i in range(ncv):
   plumed_input+=cvname[i]+': COMBINE ARG=d PERIODIC='+periodic[i]+'\n' #recreate CVs label doesn't work if they have components!
 plumed_input+='opes: '+action
-plumed_input+=' TEMP=1 PACE=1 BARRIER=0'
 plumed_input+=' ARG='+cvname[0]
 for ii in range(1,ncv):
   plumed_input+=','+cvname[ii]
@@ -91,6 +93,9 @@ plumed_input+=' BIASFACTOR='+biasfactor
 plumed_input+=' EPSILON='+epsilon
 plumed_input+=' KERNEL_CUTOFF='+kernel_cutoff
 plumed_input+=' COMPRESSION_THRESHOLD='+compression_threshold
+plumed_input+=' TEMP=1 PACE=1 BARRIER=1 SIGMA=1'
+for ii in range(1,ncv):
+  plumed_input+=',1'
 
 tmp_plumed_file='tmp-plumed_driver.dat'
 f=open(tmp_plumed_file,'w')
